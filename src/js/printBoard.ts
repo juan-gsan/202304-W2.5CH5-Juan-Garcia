89e4557b965e80f51b65c340530a45273e4eacda
JuanG <juan.gsanchez13@gmail.com>
2023-05-13 14:50:44 +0200

    Update console output function

--- src/js/printBoard.ts
+++ src/js/printBoard.ts
@@ -8,18 +8,12 @@ export const printBoard = () => {
   //   const board = createInitialBoard(3);
   // }
 
-  const randomBoard = createRandomBoard(5, 5);
+  const randomBoard = createRandomBoard(50, 50);
 
   for (let y = 0; y < randomBoard.length; y++) {
-    consoleOutput = randomBoard[y];
+    consoleOutput = randomBoard[y].join(" ");
     consoleOutput += "\n";
     console.log(consoleOutput);
-    for (let x = 0; x < randomBoard[y].length; x++) {
-      consoleOutput
-        .replaceAll(",", " ")
-        .replaceAll("[", "")
-        .replaceAll("]", "");
-    }
   }
 };
 
